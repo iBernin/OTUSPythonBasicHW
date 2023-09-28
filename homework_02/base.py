f5d87ee02cec11abbe0faa2bb05f069716dcb7da
iBernin <berninivan@gmail.com>
2023-09-28 10:28:28 +0300

    Add base.py

---
+++ homework_02/base.py
@@ -0,0 +1,42 @@
+#import sys
+#sys.path.append('C:\\Users\\gremlin\\YandexDisk\\python\\OTUSPythonBasic\\homeworks\\')
+from homework_02 import exceptions
+
+
+class Vehicle():
+    started = False
+
+    def __init__(self, weight=100, fuel=50, fuel_consumption=1):
+        self.weight = weight
+        self.fuel = fuel
+        self.fuel_consumption = fuel_consumption
+
+    def start(self):
+        if self.started is False and self.fuel > 0:
+            self.started = True
+            print('Vehicle started')
+        elif self.started is True:
+            print('Vehicle already started')
+        else:
+            raise exceptions.LowFuelError('Low Fuel')
+
+    def move(self, distance):
+        if self.fuel <= 0 and self.fuel_consumption <= 0 and self.fuel <= self.fuel_consumption:
+            raise exceptions.NotEnoughFuel('Not Enough Fuel')
+        elif self.fuel < (distance * self.fuel_consumption):
+            raise exceptions.NotEnoughFuel('Not Enough Fuel')
+        else:
+            self.fuel -= (distance * self.fuel_consumption)
+            print(f'осталось {self.fuel} топлива')
+
+
+# def main():
+#     trip = Vehicle(100, 50, 1)
+#     trip.start()
+#     trip.move(1)
+    
+
+
+
+# if __name__ == '__main__':
+#     main()
